1b7b74a14ac8f0681772d3e8fc4c3853cb551ab6
yujie1219 <709527016@qq.com>
2020-02-19 17:15:33 +0800

    102 BFS

---
+++ src/solution/LeetCode102.java
@@ -0,0 +1,36 @@
+package solution;
+
+import model.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode102 {
+
+    List<List<Integer>> results = new ArrayList<>();
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root != null) {
+            bFS(root,0);
+        }
+
+        return results;
+    }
+
+    private void bFS(TreeNode current,int level) {
+        if (results.size() == level) {
+            results.add(new ArrayList<>());
+        }
+
+        results.get(level).add(current.val);
+
+        if (current.left != null) {
+            bFS(current.left, level + 1);
+        }
+
+        if (current.right != null) {
+            bFS(current.right, level + 1);
+        }
+    }
+}
